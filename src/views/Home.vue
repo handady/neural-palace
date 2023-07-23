19eef0fd78c5b88a6689069a2cf5b80d2fd26b72
handady <1622352990@qq.com>
2023-07-23 23:59:51 +0800

    增加3d-force-graph

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,60 @@
 <template>
-    <div class="home">
-        <h1>Home</h1>
-    </div>
+    <div id="3d-graph"></div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import ForceGraph3D from '3d-force-graph';
+
 export default {
-    name: 'Home',
     setup() {
-        // 使用 ref 创建响应式的数据
-        const data = ref({
-            // 你可以在这里定义组件的数据
+        const Graph = ref(null);
+
+        onMounted(() => {
+            // Random tree
+            const N = 300;
+            const gData = {
+                nodes: [...Array(N).keys()].map(i => ({ id: i })),
+                links: [...Array(N).keys()]
+                    .filter(id => id)
+                    .map(id => ({
+                        source: id,
+                        target: Math.round(Math.random() * (id - 1))
+                    }))
+            };
+
+            Graph.value = ForceGraph3D()(document.getElementById('3d-graph'))
+                .graphData(gData)
+                .onNodeClick(node => {
+                    // Aim at node from outside it
+                    const distance = 40;
+                    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+                    const newPos = node.x || node.y || node.z
+                        ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+                        : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+
+                    Graph.value.cameraPosition(
+                        newPos, // new position
+                        node, // lookAt ({ x, y, z })
+                        3000  // ms transition duration
+                    );
+                });
         });
 
         return {
-            data,
-            // 你的组件的方法可以在这里定义
+            Graph,
         };
-    }
+    },
 }
 </script>
 
-<style></style>
+<style scoped>
+#3d-graph {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+</style>
